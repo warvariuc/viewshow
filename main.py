ff93a8159d888eaad38d041de86f371f7dde44dd
Victor Varvaryuk <v.varvariuc@corp.mail.ru>
2013-04-08 14:18:17 +0400

    .

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ import pexpect
 QtCore.pyqtRemoveInputHook()
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-FormClass, BaseClass = uic.loadUiType('main.ui')
+FormClass, BaseClass = uic.loadUiType(os.path.join(BASE_DIR, 'main.ui'))
 assert BaseClass is QtGui.QDialog
 
 
